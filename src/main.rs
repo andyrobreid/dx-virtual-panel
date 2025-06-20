533ab71eac3705f3ec5c4fb01d899219e953fa7b
andyrobreid <andyrobreid@gmail.com>
2025-06-20 23:00:13 +0100

    feat: 2 input types atm digital and analog

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,35 @@ struct SignalProps {
     tooltip: String,
 }
 
+#[derive(Props, PartialEq, Clone)]
+struct DigitalSignal {
+    base: SignalProps,
+    value: bool,
+}
+
+#[derive(Props, PartialEq, Clone)]
+struct AnalogSignal {
+    base: SignalProps,
+    value: u32,
+}
+
+#[derive(Clone)]
+enum SignalKind {
+    Digital(DigitalSignal),
+    Analog(AnalogSignal),
+}
+
+#[derive(Clone)]
+struct State {
+    signals: Vec<SignalKind>,
+}
+
+fn ConfigureState() -> Result<(), &'static str> {
+    // Get configuration of state from a config yaml file
+    todo!();
+    Ok(())
+}
+
 #[component]
 fn Title() -> Element {
     rsx! {
@@ -17,10 +46,16 @@ fn Title() -> Element {
 }
 
 #[component]
-fn DigitalSignal(props: SignalProps) -> Element {
+fn DigitalSignal(props: DigitalSignal) -> Element {
     rsx!(
-        button { title: "{props.tooltip}" , "{props.name}" }
+        button { title: "{props.base.tooltip}" , "{props.base.name}" }
+    )
+}
 
+#[component]
+fn AnalogSignal(props: AnalogSignal) -> Element {
+    rsx!(
+        input { title: "{props.base.tooltip}" , "{props.base.name}" }
     )
 }
 
@@ -34,9 +69,17 @@ fn Panel() -> Element {
             },
             button { onclick: move |event| tracing::debug!{"Clicked Event {event:?}"}, id: "save", "save!" },
             DigitalSignal{
-                name: "TEST",
-                tooltip: "this is a tooltip"
+                base: SignalProps {
+                    name: String::from("TEST"),
+                    tooltip: String::from("this is a Digial Input")
+                },
+                value: true
+            },
+            AnalogSignal{
+                base: SignalProps { name: String::from("Test Analog"), tooltip: String::from("This is a analog input") },
+                value: 100
             }
+
         }
     }
 }
