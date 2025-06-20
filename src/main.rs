ce69f202e9e2c9aaa72c1bf6cd1005ef12468a50
andyrobreid <andyrobreid@gmail.com>
2025-06-21 00:23:01 +0100

    clean: removed code from tutorial

--- src/main.rs
+++ src/main.rs
@@ -106,13 +106,6 @@ fn Panel() -> Element {
         }
     });
     rsx! {
-        div { id: "buttons",
-            button {
-                onclick: move |event| tracing::debug!{"Clicked skip button with {event:?}"},
-                id: "skip", "skip"
-            },
-            button { onclick: move |event| tracing::debug!{"Clicked Event {event:?}"}, id: "save", "save!" },
-        }
         div {
             id: "inputs",
             {signals_rendered}
